03a33173dd9321d83bcd8d4cac18b9bb52120bef
matisapatta <mati.sapatta@gmail.com>
2020-03-03 20:33:16 -0300

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*
 Dependencias
  */
-const express = require('express');
+/*const express = require('express');
 
 const app = express();
 
@@ -20,6 +20,28 @@ app.get('/api/punchoutget', function (req, res) {
     console.log("punchout post")
     console.log(req)
     res.send("punchout post");
-  });
+  });*/
+
+  var request = require('request');
+  var xml2js = require('xml2js');
+  
+  var sendJsonResponse = function(res, status, content) {
+      res.status(status);
+      res.json(content);
+  };
+  
+  /* GET XML Content*/
+  module.exports.dsRequest = function(req, res) {
+  
+      var parser = new xml2js.Parser();
+  
+      request('/api/punchoutpost', function(error, response, body) {
+          parser.parseString(body, function(err, result) {
+              sendJsonResponse(res, 200, result);
+          });
+  
+      });
+  
+  };
 
   
