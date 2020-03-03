9d430dfe526b320b993ca1c4389bb8387d71c34d
matisapatta <mati.sapatta@gmail.com>
2020-03-03 20:14:08 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+/*
+Dependencias
+ */
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3008;
+app.listen(port, () => {
+  console.log("Server running on port", port)
+})
+
+app.get('/api/punchoutget', function (req, res) {
+    console.log("punchout get")
+    console.log(req)
+    res.send("punchout get");
+  });
+  
+  app.post('/api/punchoutpost', function (req, res) {
+    console.log("punchout post")
+    console.log(req)
+    res.send("punchout post");
+  });
+
+  
