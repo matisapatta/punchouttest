8bb46c2d06316a6825cdb8385b89e92bb140fe4c
matisapatta <mati.sapatta@gmail.com>
2020-03-03 20:58:08 -0300

    .

--- server.js
+++ server.js
@@ -23,8 +23,21 @@ app.get('/api/punchoutget', function (req, res) {
     console.log(req)
     res.type('application/xml');
 
-    res.send("<?xml version=\"1.0?\"\>"
-    +"<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\"\>"
+    // res.send("<?xml version=\"1.0?\"\>"
+    // +"<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\"\>"
+    // +"<cXML xml:lang=\"en-US\" payloadID=\"933694607739 \" timestamp=\"2017-03-14T12:59:09-07:00\"\>"
+    //   +"<Response\>"
+    //   +"<Status code=\"200\" text=\"success\"\></Status\>"
+    //     +"<PunchOutSetupResponse\>"
+    //      + "<StartPage\>"
+    //         +"<URL>https://punchout.workchairs.com/Servlet/sessionid=7006</URL\>"
+    //       +"</StartPage\>"
+    //     +"</PunchOutSetupResponse\>"
+    //   +"</Response\>"
+    // +"</cXML\>"
+    // );
+        res.send(
+    "<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\"\>"
     +"<cXML xml:lang=\"en-US\" payloadID=\"933694607739 \" timestamp=\"2017-03-14T12:59:09-07:00\"\>"
       +"<Response\>"
       +"<Status code=\"200\" text=\"success\"\></Status\>"
