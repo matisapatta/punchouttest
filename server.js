4c769a7b3063959cbb62750901dc629c194bfdec
matisapatta <mati.sapatta@gmail.com>
2020-03-03 20:52:04 -0300

    .

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 
 const app = express();
 
+var xml = require('xml');
+
 const port = process.env.PORT || 3008;
 app.listen(port, () => {
   console.log("Server running on port", port)
@@ -19,7 +21,9 @@ app.get('/api/punchoutget', function (req, res) {
   app.post('/api/punchoutpost', function (req, res) {
     console.log("punchout post")
     console.log(req)
-    res.send("<?xml version=\"1.0?\"\>"
+    res.set('Content-Type', 'text/xml');
+
+    res.send(xml("<?xml version=\"1.0?\"\>"
     +"<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\"\>"
     +"<cXML xml:lang=\"en-US\" payloadID=\"933694607739 \" timestamp=\"2017-03-14T12:59:09-07:00\"\>"
       +"<Response\>"
@@ -31,7 +35,7 @@ app.get('/api/punchoutget', function (req, res) {
         +"</PunchOutSetupResponse\>"
       +"</Response\>"
     +"</cXML\>"
-    );
+    ));
   });
 
   
