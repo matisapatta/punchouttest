d667e9b6c4041f98221d4ea9d631d4073fc1651b
matisapatta <mati.sapatta@gmail.com>
2020-03-03 20:47:23 -0300

    .

--- server.js
+++ server.js
@@ -19,7 +19,19 @@ app.get('/api/punchoutget', function (req, res) {
   app.post('/api/punchoutpost', function (req, res) {
     console.log("punchout post")
     console.log(req)
-    res.status(200);
+    res.send("<?xml version=\"1.0?\"\>"
+    +"<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\"\>"
+    +"<cXML xml:lang=\"en-US\" payloadID=\"933694607739 \" timestamp=\"2017-03-14T12:59:09-07:00\"\>"
+      +"<Response\>"
+      +"<Status code=\"200\" text=\"success\"\></Status\>"
+        +"<PunchOutSetupResponse\>"
+         + "<StartPage\>"
+            +"<URL>https://punchout.workchairs.com/Servlet/sessionid=7006</URL\>"
+          +"</StartPage\>"
+        +"</PunchOutSetupResponse\>"
+      +"</Response\>"
+    +"</cXML\>"
+    );
   });
 
   
